b3d5c931f91d6ba3b576c8961dba6fbaf871d704
Omar Alkaseh <omarelkasihemail@example.com>
2026-09-04 20:23:58 +0200

    feat: implement mapping as genaric map

--- src/Api/Features/VehicleMakes/Requests/CreateVehicleMakeRequest.cs
+++ src/Api/Features/VehicleMakes/Requests/CreateVehicleMakeRequest.cs
@@ -1,6 +1,5 @@
-﻿namespace Car_Rental_Management.Features.VehicleMakes.Requests
-{
+﻿namespace Car_Rental_Management.Features.VehicleMakes.Requests;
+
     public sealed record CreateVehicleMakeRequest(
         string vehicleMakeName);
 
-}
---
+++ src/Api/Features/VehicleMakes/Responses/VehicleMakeResponse.cs
@@ -0,0 +1,3 @@
+namespace CarRental.Application.Features.VehicleMakes.Responses;
+
+public sealed record VehicleMakeResponse(int Id, string Name, bool IsActive);
